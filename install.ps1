d55e18e400f491c6736d5271a54b56b5d3ff0f72
Kshitij Aucharmal <kshitijaucharmal21@gmail.com>
2024-01-18 19:29:24 +0530

    Added install.ps1 for windoze users

---
+++ install.ps1
@@ -0,0 +1,34 @@
+# Install using venv
+If (-not (Test-Path target)) {
+    Write-Host "Installing..."
+    python -m venv env
+    env\Scripts\Activate.ps1
+    pip install -r requirements.txt
+    pip install pyinstaller
+    pyinstaller "src/__main__.py" --clean -n "geminishell" --distpath target
+    env\Scripts\Deactivate.ps1
+
+    # Remove redundant
+    Remove-Item -Recurse env build geminishell.spec
+    Write-Host "Installed."
+}
+else {
+    Write-Host "target exists."
+}
+
+# Add to path
+$env:Path += ";$(Get-Location)\target\geminishell"
+
+# Config setup
+If (-not (Test-Path "$HOME\.config\geminishell\config.toml")) {
+    Write-Host "Config file doesn't exist"
+    Write-Host "Enter API Key (https://makersuite.google.com/app/apikey): "
+    $GOOGLE_API_KEY = Read-Host
+
+    mkdir "$HOME\.config\geminishell"
+    (Get-Content config.toml) -replace '<your api key>', $GOOGLE_API_KEY | Set-Content "$HOME\.config\geminishell\config.toml"
+
+    Write-Host "Created config file"
+}
+
+Write-Host "Done."
