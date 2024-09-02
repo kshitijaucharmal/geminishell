27d589236b05bf135b4d26b0ed7e4f3cca547f68
KshitijAucharmal <kshitijaucharmal21@gmail.com>
2024-09-02 18:45:58 +0530

    Added support in install.sh to open in browser

--- install.sh
+++ install.sh
@@ -21,10 +21,16 @@ fi
 echo "Linking to .local/bin"
 ln -sf "$(pwd)/target/geminishell/geminishell" ~/.local/bin/geminishell
 
+URL="https://makersuite.google.com/app/apikey"
+
 # Config setup
 if [ ! -f "$HOME/.config/geminishell/config.toml" ]; then
     echo "Config file doesn't exist"
-    echo "Enter API Key (https://makersuite.google.com/app/apikey): "
+    echo "Please create an API Key and copy-paste it in the terminal"
+    echo "Press \"Enter\" to open ($URL) in $BROWSER: "
+    read -n 1
+    $BROWSER $URL
+    echo "Enter API Key: "
     read GOOGLE_API_KEY
 
     mkdir -p "$HOME/.config/geminishell"
